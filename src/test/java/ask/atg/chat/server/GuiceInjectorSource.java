83bec06568e7f86248a13012ca51eed19b476eee
ask-skoogiz <anders.skoglund@skoogiz.com>
2016-10-05 12:11:36 +0200

    Guice configuration for dependency injection

---
+++ src/test/java/ask/atg/chat/server/GuiceInjectorSource.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package ask.atg.chat.server;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Stage;
+
+import cucumber.api.guice.CucumberModules;
+import cucumber.runtime.java.guice.InjectorSource;
+
+/**
+ * Guice {@link InjectorSource} for dependency injection while testing the chat server implementation.
+ * 
+ * @author Anders Skoglund
+ *
+ */
+public class GuiceInjectorSource implements InjectorSource
+{
+
+    @Override
+    public Injector getInjector()
+    {
+        return Guice.createInjector(Stage.DEVELOPMENT, CucumberModules.SCENARIO, new GuiceModule());
+    }
+}
---
+++ src/test/java/ask/atg/chat/server/GuiceModule.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package ask.atg.chat.server;
+
+import ask.atg.chat.server.logic.ChatCache;
+import ask.atg.chat.server.logic.ChatDao;
+import ask.atg.chat.server.logic.ContactCache;
+import ask.atg.chat.server.logic.ContactDao;
+import ask.atg.chat.server.logic.UserCache;
+import ask.atg.chat.server.logic.UserDao;
+import ask.atg.chat.server.service.ChatService;
+import ask.atg.chat.server.service.ChatServiceImpl;
+
+import com.google.inject.AbstractModule;
+
+import cucumber.runtime.java.guice.InjectorSource;
+
+/**
+ * Guice {@link InjectorSource} for dependency injection while testing the chat server implementation.
+ * 
+ * @author Anders Skoglund
+ *
+ */
+public class GuiceModule extends AbstractModule
+{
+    public void configure()
+    {
+        bind(ChatService.class).to(ChatServiceImpl.class);
+        bind(UserDao.class).to(UserCache.class);
+        bind(ContactDao.class).to(ContactCache.class);
+        bind(ChatDao.class).to(ChatCache.class);
+    }
+}
